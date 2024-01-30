0c13f1b0f9edccd734213c379ae3f65d675cc98c
KaspianTC <kaspian.thoft.christensen@mail.utoronto.ca>
2024-01-29 23:29:43 -0500

    deleteNote works

--- backend-example/server.js
+++ backend-example/server.js
@@ -162,3 +162,89 @@ app.get("/getNote/:noteId", express.json(), async (req, res) => {
       res.status(500).json({ error: error.message });
     }
   });
+app.get("/getAllNotes", express.json(), async (req, res) => {
+    try {  
+      // Verify the JWT from the request headers
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, "secret-key", async (err, decoded) => {
+        if (err) {
+          return res.status(401).send("Unauthorized.");
+        }
+  
+        // Find note with given ID
+        const collection = db.collection(COLLECTIONS.notes);
+        const data = await collection.find({username: decoded.username});
+        res.json({ response: data });
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+app.delete("/deleteNote/:noteId", express.json(), async (req, res) => {
+    try {
+      // Basic param checking
+      const noteId = req.params.noteId;
+      if (!ObjectId.isValid(noteId)) {
+        return res.status(400).json({ error: "Bad request in relation to the :noteId URL parameter" });
+      }
+  
+      // Verify the JWT from the request headers
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, "secret-key", async (err, decoded) => {
+        if (err) {
+          return res.status(401).send("Unauthorized.");
+        }
+  
+        // Find note with given ID
+        const collection = db.collection(COLLECTIONS.notes);
+        const data = await collection.findOneAndDelete({
+          username: decoded.username,
+          _id: new ObjectId(noteId),
+        });
+        if (!data) {
+          return res
+            .status(404)
+            .json({ error: "Note with ID noteId belonging to the user not found" });
+        }
+        res.json({ response: "Document with ID " + noteId + "properly deleted" });
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+app.patch("/editNote/:noteId", express.json(), async (req, res) => {
+    try {
+      // Basic param checking
+      const { title, content } = req.body;
+      const noteId = req.params.noteId;
+      if (!ObjectId.isValid(noteId) || !title || !content) {
+        return res.status(400).json({ error: "Bad request in relation to the :noteId URL parameter, or the body params" });
+      }
+  
+      // Verify the JWT from the request headers
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, "secret-key", async (err, decoded) => {
+        if (err) {
+          return res.status(401).send("Unauthorized.");
+        }
+  
+        // Find note with given ID
+        const collection = db.collection(COLLECTIONS.notes);
+        const data = await collection.findOneAndDelete({
+          username: decoded.username,
+          title: title,
+          content: content,
+          _id: new ObjectId(noteId),
+        });
+        if (!data) {
+          return res
+            .status(404)
+            .json({ error: "Note with ID noteId belonging to the user not found" });
+        }
+        res.json({ response: "Document with ID " + noteId + "properly updated" });
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
